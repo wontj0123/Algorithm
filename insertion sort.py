fce9ed132f11d52d596992ea7023fe7f08d97c9a
wontj0123 <xowns150@gmail.com>
2021-02-21 22:46:18 +0900

    퀵정렬 p168

--- insertion sort.py
+++ insertion sort.py
@@ -1,7 +1,6 @@
 arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
 for i in range(len(arr)):
-    min_index = i
     for j in range(i, 0, -1):
         if arr[j]<arr[j-1]:
             arr[j], arr[j-1] = arr[j-1], arr[j]
