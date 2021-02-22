edf81a6fc08b65467c2c2e5582181059b2ce5adc
wontj0123 <xowns150@gmail.com>
2021-02-22 22:20:14 +0900

    위에서 아래로 정렬 수정

--- up_side_down-p179.py
+++ up_side_down-p179.py
@@ -1,13 +1,15 @@
-from collections import deque
 import sys
 from os import kill
 sys.stdin=open("up_side_down.txt","rt")
-
+#N을 입력받기
 n= int(input())
+
+#N개의 정수를 입력받아 리스트에 저장
 arr = []
-print(n)
 for i in range(n):
     arr.append(int(input()))
-arr.sorted(reverse=True)
+#파이썬 기본 정렬 라이브러리를 이용하여 정렬 수행
+arr.sort(reverse=True)
+#정렬이 수행된 결과를 출력
 for i in arr:
     print(i,end=" ")
