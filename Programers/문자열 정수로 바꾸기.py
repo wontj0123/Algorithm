59589052b19b2983f5c674fac221ba65fced3015
wontj0123 <xowns150@gmail.com>
2021-02-28 15:42:33 +0900

    문자열 정수로 바꾸기

---
+++ Programers/문자열 정수로 바꾸기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    result = 0
+    for idx, number in enumerate(s[::-1]):
+        if number == '-':
+            result *= -1
+        else:
+            result += int(number)*(10**idx)
+    return result
