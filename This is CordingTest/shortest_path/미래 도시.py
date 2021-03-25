6faab002546e1d1c2a523abf5dc49f5228885dff
wontj0123 <xowns150@gmail.com>
2021-03-25 21:50:49 +0900

    미래도시

--- This is CordingTest/shortest_path/미래 도시.py
+++ This is CordingTest/shortest_path/미래 도시.py
@@ -1,10 +1,13 @@
+import sys
+sys.stdin=open("미래 도시.txt", "rt")
+
 INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
 
 # 노드의 개수 및 간선의 개수를 입력받기
 n, m = map(int, input().split())
 # 2차원 리스트(그래프 표현)를 만들고, 모든 값을 무한으로 초기화
 graph = [[INF] * (n + 1) for _ in range(n + 1)]
-
+print(graph)
 # 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
 for a in range(1, n + 1):
     for b in range(1, n + 1):
