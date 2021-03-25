92cd0f0406789a4c1156f00c71d7eb6df9e9bf38
wontj0123 <xowns150@gmail.com>
2021-03-25 21:22:00 +0900

    개선된 다익스트라

--- This is CordingTest/shortest_path/new_dijkstra.py
+++ This is CordingTest/shortest_path/new_dijkstra.py
@@ -2,8 +2,8 @@ import heapq
 import sys
 sys.stdin=open("dijkstra.txt", "rt")
 input = sys.stdin.readline
-INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
 
+INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
 # 노드의 개수, 간선의 개수를 입력받기
 n, m = map(int, input().split())
 # 시작 노드 번호를 입력받기
@@ -18,7 +18,6 @@ for _ in range(m):
     a, b, c = map(int, input().split())
     # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
     graph[a].append((b, c))
-    print(graph)
 
 def dijkstra(start):
     q = []
@@ -27,17 +26,18 @@ def dijkstra(start):
     distance[start] = 0
     while q: # 큐가 비어있지 않다면
         # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        #가중치,노드 위치 인덱스
         dist, now = heapq.heappop(q)
-        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시(이미 더 작은 값 들어있음)
         if distance[now] < dist:
             continue
         # 현재 노드와 연결된 다른 인접한 노드들을 확인
-        for i in graph[now]:
-            cost = dist + i[1]
+        for i in graph[now]:#그래프에서 now와 연결된거 i로 차례대로 탐색
+            cost = dist + i[1]#현재 가중치와 기록된 가중치 더해서 대입
             # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
             if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+                distance[i[0]] = cost#더 짧은 가중치로 기록
+                heapq.heappush(q, (cost, i[0]))#큐에 삽입
 
 # 다익스트라 알고리즘을 수행
 dijkstra(start)
--- This is CordingTest/shortest_path/플로이드 워셜.py
+++ This is CordingTest/shortest_path/플로이드 워셜.py
@@ -1,3 +1,6 @@
+import sys
+sys.stdin=open("Floyd-Warshall", "rt")
+
 INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
 
 # 노드의 개수 및 간선의 개수를 입력받기
